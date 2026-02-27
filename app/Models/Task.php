2e8ee89b784eac484b2c4010dfdc4bd6b882f6f9
Isaac N Reis <isaacreisn@gmail.com>
2026-02-26 22:54:41 -0300

    feat: configura mass assignment e casts no model Task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'completed',
+    ];
+
+    protected $casts = [
+        'completed' => 'boolean',
+    ];
 }
